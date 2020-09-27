bd95f3d5c15c26a5ab3da8ac64448a8c56466b0c
Brian <brian@Brians-MacBook-Air.local>
2020-09-26 23:17:41 -0600

    added add new list button

--- lib/ui/views/list_view/list_view.dart
+++ lib/ui/views/list_view/list_view.dart
@@ -3,6 +3,7 @@ import 'package:stacked/stacked.dart';
 import 'package:taskbuddies/app/locator.dart';
 import 'package:taskbuddies/models/todo_list.dart';
 import 'package:taskbuddies/ui/views/list_view/list_view_model.dart';
+import 'package:taskbuddies/ui/widgets/helpers.dart';
 import 'package:taskbuddies/ui/widgets/list_widget.dart';
 
 class TodoListView extends StatelessWidget {
@@ -19,10 +20,29 @@ class TodoListView extends StatelessWidget {
     print('hello from todo list view');
     return ViewModelBuilder.reactive(
       disposeViewModel: false,
-      builder: (context, model, child) => Container(
-        child: ListView(
-          padding: EdgeInsets.fromLTRB(0, 0, 0, 8),
-          children: addListToWidget(model.todoLists),
+      builder: (context, model, child) => SingleChildScrollView(
+        child: Column(
+          children: [
+            Container(
+              child: ListView(
+                shrinkWrap: true,
+                padding: EdgeInsets.fromLTRB(0, 0, 0, 8),
+                children: addListToWidget(model.todoLists),
+              ),
+            ),
+            smallVertSpace(),
+            RaisedButton(
+              color: Colors.black26,
+              onPressed: () {},
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.all(Radius.circular(16)),
+              ),
+              child: Text(
+                'Make a new List',
+                style: TextStyle(color: Colors.white),
+              ),
+            )
+          ],
         ),
       ),
       viewModelBuilder: () => locator<TodoListViewModel>(),
